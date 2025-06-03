2b8f2b01b261a7ed27e092e0e9b66b41f88d20a1
Jaime Suarez <jaisua8@gmail.com>
2025-06-02 22:38:50 -0500

    actualizacion de las entidades mas sus getters y setters

--- src/main/java/com/proyecto/panaderialosandes/models/Productos.java
+++ src/main/java/com/proyecto/panaderialosandes/models/Productos.java
@@ -1,5 +1,28 @@
 package com.proyecto.panaderialosandes.models;
 
+import jakarta.persistence.Entity;  
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Productos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String categoria;
+    private int cantidad;
+    private double precio;
+    private int unidad;
     
+   
 }
---
+++ src/main/java/com/proyecto/panaderialosandes/services/ProductoValidadorService.java
@@ -0,0 +1,31 @@
+package com.proyecto.panaderialosandes.services;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.proyecto.panaderialosandes.models.Productos;    
+
+public class ProductoValidadorService {
+
+    public void validarProducto(String nombre, double precio, int cantidad, String categoria, int unidad) {
+        Preconditions.checkNotNull(nombre, "El nombre del producto no puede ser nulo.");
+        Preconditions.checkArgument(precio > 0, "El precio debe ser mayor a 0.");
+        Preconditions.checkArgument(cantidad >= 0, "La cantidad no puede ser negativa.");
+        Preconditions.checkNotNull(categoria, "La categoria no puede ser nula.");
+        Preconditions.checkArgument(unidad > 0, "La unidad no puede ser negativa.");
+    }
+
+    public void productosPorCategoria(Productos producto) {   
+
+        Multimap<String, String> productosPorCategoria = ArrayListMultimap.create();
+        
+        productosPorCategoria.put(producto.getCategoria(), producto.getNombre());
+        productosPorCategoria.put("Panadería", "Pan Integral");
+        productosPorCategoria.put("Pastelería", "Torta de Chocolate");
+        productosPorCategoria.put("Pastelería", "Tiramisú");
+
+        System.out.println(productosPorCategoria);
+    }
+}
+
+    
+
